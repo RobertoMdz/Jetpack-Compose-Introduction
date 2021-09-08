aa95984a3a7ae55b89e681092ccff41468bbd32b
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-09-08 13:38:04 -0500

    implement counter

--- app/src/main/java/com/roberthmdz/jetpackcomposeintroduction/MainActivity.kt
+++ app/src/main/java/com/roberthmdz/jetpackcomposeintroduction/MainActivity.kt
@@ -5,15 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.material.Checkbox
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.roberthmdz.jetpackcomposeintroduction.ui.theme.JetpackComposeIntroductionTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,6 +28,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SimpleComposable() {
+    var num by remember { mutableStateOf(0) }
+
+    Column {
+        
+        Text(text = "Conteo: $num")
+        Divider(thickness = 20.dp, color = Color.Transparent)
+        Button(
+            onClick = { num++ }
+        ) {
+            Text(text = "Actualizar conteo")
+        }
+
+    }
 
 
 }
