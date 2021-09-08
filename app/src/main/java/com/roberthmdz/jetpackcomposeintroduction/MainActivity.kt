f7987851eb558c70c1b1db291d080900a84f068e
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-09-08 12:12:26 -0500

    Implement column and rows

--- app/src/main/java/com/roberthmdz/jetpackcomposeintroduction/MainActivity.kt
+++ app/src/main/java/com/roberthmdz/jetpackcomposeintroduction/MainActivity.kt
@@ -3,8 +3,9 @@ package com.roberthmdz.jetpackcomposeintroduction
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,8 +13,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.roberthmdz.jetpackcomposeintroduction.ui.theme.JetpackComposeIntroductionTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,22 +31,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun Figure(color: Color) {
+    Box(
+        modifier = Modifier
+            .width(50.dp)
+            .height(50.dp)
+            .clip(CircleShape)
+            .background(color)
+    )
+}
+
+@Composable
 fun SimpleComposable() {
-    val checkBoxState = remember{ mutableStateOf(false) }
+
+
+
+    Row(
+        modifier = Modifier
+        .fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Figure(color = Color.Red)
+        Figure(color = Color.Green)
+        Figure(color = Color.Blue)
+    }
 
     Column(
-        modifier = Modifier.fillMaxHeight()
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-        Text(text = "Hola mundo")
-        Checkbox(
-            checked = checkBoxState.value,
-            onCheckedChange = {
-                checkBoxState.value = !checkBoxState.value
-            }
-        )
-
+        Figure(color = Color.Red)
+        Figure(color = Color.Green)
+        Figure(color = Color.Blue)
     }
+
+
+
 
 }
 
